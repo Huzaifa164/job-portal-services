67beb0ccd18b7c688d92ea9e97ab1d0374df1246
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2026-02-02 19:41:22 +0530

    implemented auth middleware and get profile functionality in users service

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`User service is running on http://localhost:${process.env.PORT}`);
