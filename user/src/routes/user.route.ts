95ed8b097ddeb5631ed6c1145c4aaa4ae59157af
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2026-02-02 20:29:24 +0530

    implemented update profile pic functionality

--- user/src/routes/user.route.ts
+++ user/src/routes/user.route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuth } from '../middlewares/auth.js';
-import { getUserProfile, myProfile, updateUserProfile } from '../controllers/user.controller.js';
+import { getUserProfile, myProfile, updateProfilePic, updateUserProfile } from '../controllers/user.controller.js';
+import uploadFile from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
 
 export default router;
