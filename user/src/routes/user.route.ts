f7fb68e3588e71a904962bc5e809a596c2f91823
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2026-02-02 20:03:19 +0530

    implemented update-profile functionality

--- user/src/routes/user.route.ts
+++ user/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { isAuth } from '../middlewares/auth.js';
-import { getUserProfile, myProfile } from '../controllers/user.controller.js';
+import { getUserProfile, myProfile, updateUserProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
+router.put("/update/profile", isAuth, updateUserProfile);
 
 export default router;
