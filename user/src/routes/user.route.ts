61380aba0d7e4c6153848e1bec4e4d32af070bef
Huzaifa164 <ansarihuzaifa654321@gmail.com>
2026-02-03 19:49:44 +0530

    implemented update resume functionality

--- user/src/routes/user.route.ts
+++ user/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middlewares/auth.js';
-import { getUserProfile, myProfile, updateProfilePic, updateUserProfile } from '../controllers/user.controller.js';
+import { getUserProfile, myProfile, updateProfilePic, updateResume, updateUserProfile } from '../controllers/user.controller.js';
 import uploadFile from '../middlewares/multer.js';
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
 router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
+router.put("/update/resume", isAuth, uploadFile, updateResume);
 
 export default router;
